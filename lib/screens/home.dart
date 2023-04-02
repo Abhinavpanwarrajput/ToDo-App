8cb0888c48612e13d0a05b9823a3c67bbd54cfb1
Abhinav <abhinavrajput207@gmail.com>
2023-04-02 17:01:08 +0530

    initial commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:todo/constants/colors.dart';
+import 'package:todo/model/todo.dart';
+import 'package:todo/widgets/todo_item.dart';
+
+class Home extends StatefulWidget {
+  Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final todoList = ToDo.todoList();
+  final _todoController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 50, bottom: 20),
+                        child: const Text(
+                          'All ToDos',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      for (ToDo todoo in todoList)
+                        ToDoItem(
+                          todo: todoo,
+                          onToDoChanged: _handelToDoChange,
+                          onDeleteItem: _deleteToDoItem,
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                          bottom: 20, right: 20, left: 20),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 0.0,
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TextField(
+                        controller: _todoController,
+                        decoration: const InputDecoration(
+                            hintText: 'Add a new ToDo item',
+                            border: InputBorder.none),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 20, right: 20),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          _addToDoItem(_todoController.text);
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: tdBlue,
+                            minimumSize: const Size(60, 60),
+                            elevation: 10),
+                        child: const Text(
+                          '+',
+                          style: TextStyle(
+                            fontSize: 40,
+                          ),
+                        )),
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+
+  void _handelToDoChange(ToDo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
+
+  void _deleteToDoItem(String id) {
+    setState(() {
+      todoList.removeWhere((item) => item.id == id);
+    });
+  }
+
+  void _addToDoItem(String toDo) {
+    setState(() {
+      todoList.add(ToDo(
+          id: DateTime.now().millisecondsSinceEpoch.toString(),
+          todoText: toDo));
+    });
+    _todoController.clear();
+  }
+
+  // search Box
+  Widget searchBox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: const TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, maxWidth: 25),
+          border: InputBorder.none,
+          hintText: 'Search Text',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
+      ),
+    );
+  }
+
+  // AppBar
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          // Container(
+          //   height: 40,
+          //   width: 40,
+          //   child: ClipRRect(borderRadius: BorderRadius.circular(20), child: Image.asset('assets/images/demo.jpeg')),
+          // ),
+        ],
+      ),
+    );
+  }
+}
